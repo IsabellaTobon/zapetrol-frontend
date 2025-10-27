14518af73841ea731cee6415ffa93feb8c0d37b7
Isabella T. <89875811+IsabellaTobon@users.noreply.github.com>
2025-10-27 19:33:37 +0100

    Add Sign in modal and change css

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,18 +1,24 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import LoginModal from '../ui/Modal/LoginModal'
+import SignInModal from '../ui/Modal/SignInModal'
 
 export default function Navbar() {
 const [showLoginModal, setShowLoginModal] = useState(false)
+const [isRegistering, setIsRegistering] = useState(false);
+
 
   return (
     <>
       <nav className="navbar">
         <h1>Zapetrol Navbar</h1>
+        {/* Botones */}
         <button onClick={() => setShowLoginModal(true)}>Login</button>
+        <button onClick={() => setIsRegistering(true)}>Registrarse</button>
       </nav>
 
       {showLoginModal && <LoginModal onClose={() => setShowLoginModal(false)} />}
+      {isRegistering && <SignInModal onClose={() => setIsRegistering(false)} />}
     </>
   )
 }
--- src/components/ui/Modal/SignInModal.tsx
+++ src/components/ui/Modal/SignInModal.tsx
@@ -1,33 +1,36 @@
 import React from 'react'
 import './Log-Register-Modal.css'
 
-
-interface LoginModalProps {
+interface SignInModalProps {
   onClose?: () => void
 }
 
-interface LoginData {
+interface SignInData {
+  name: string
   email: string
   password: string
+  confirmPassword: string
 }
 
-function LoginModal({ onClose }: LoginModalProps) {
+function SignInModal({ onClose }: SignInModalProps) {
   return (
     <div className='modal-overlay'>
       <div className='modal'>
         <button className='close-btn' onClick={onClose}>X</button>
 
-        <h2>Iniciar sesión</h2>
+        <h2>Registro</h2>
 
         <form>
+          <input type="text" placeholder="Nombre" required />
           <input type="email" placeholder="Email" required />
           <input type="password" placeholder="Password" required />
+          <input type="password" placeholder="Confirm Password" required />
 
-          <button type="submit">Iniciar sesión</button>
+          <button type="submit">Registrarse</button>
         </form>
       </div>
     </div>
   )
 }
 
-export default LoginModal
+export default SignInModal
